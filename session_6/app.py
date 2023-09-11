c59cdb90e1e533faee488ed167f531febd189113
Nicholas Shellabarger <nickat9t@gmail.com>
2023-09-11 00:22:29 +0000

    add session 6

---
+++ session_6/app.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+
+from typing import Final
+
+from beaker import *
+from pyteal import *
+
+import pyteal as pt
+
+
+class RollupState:
+    manager: Final[GlobalStateValue] = GlobalStateValue(
+        stack_type=TealType.bytes,
+        default=Global.creator_address(),
+        descr="Manager"
+    )
+    url: Final[GlobalStateValue] = GlobalStateValue(
+        stack_type=TealType.bytes,
+        default=pt.Bytes(""),
+        descr="Url"
+    )
+    reserve: Final[GlobalStateValue] = GlobalStateValue(
+        stack_type=TealType.bytes,
+        default=Global.creator_address(),
+        descr="Reserve"
+    )
+    note: Final[GlobalStateValue] = GlobalStateValue(
+        stack_type=TealType.bytes,
+        default=pt.Bytes(""),
+        descr="Note"
+    )
+
+
+app = Application("Rollup", state=RollupState)
+
+
+@Subroutine(TealType.none)
+def pay(receiver: Expr, amount: Expr, asset: Expr) -> Expr:
+    """ pay asa """
+    return InnerTxnBuilder.SetFields(
+        {
+            TxnField.type_enum: TxnType.AssetTransfer,
+            TxnField.asset_receiver: receiver,
+            TxnField.asset_amount: amount,
+            TxnField.xfer_asset: asset,
+            TxnField.fee: Int(0),  # cover fee with outer txn
+        }
+    )
+
+
+@Subroutine(TealType.none)
+def pay_with_close_remainder_to(receiver: Expr, amount: Expr, asset: Expr, close_remainder_to: Expr) -> Expr:
+    """ pay asa with close remainder to """
+    return InnerTxnBuilder.SetFields(
+        {
+            TxnField.type_enum: TxnType.AssetTransfer,
+            TxnField.asset_receiver: receiver,
+            TxnField.asset_amount: amount,
+            TxnField.xfer_asset: asset,
+            TxnField.fee: Int(0),
+            TxnField.asset_close_to: close_remainder_to
+        }
+    )
+
+
+@Subroutine(TealType.none)
+def free_app_mbr() -> Expr:
+    """ free application minimum balance requirement """
+    return InnerTxnBuilder.SetFields(
+        {  # send MBR to creator_address
+            TxnField.type_enum: TxnType.Payment,
+            TxnField.fee: Int(0),  # cover fee with outer txn
+            TxnField.receiver: app.state.manager.get(),
+            # close_remainder_to to sends full balance, including 0.1 account MBR
+            TxnField.close_remainder_to: app.state.manager.get(),
+            # we are closing the account, so amount can be zero
+            TxnField.amount: Int(0),
+        }
+    )
+
+
+@Subroutine(TealType.none)
+def opt_in(asset_id: Expr) -> Expr:
+    """ optin single asset """
+    return InnerTxnBuilder.SetFields(
+        {
+            TxnField.type_enum: TxnType.AssetTransfer,
+            TxnField.fee: Int(0),  # cover fee with outer txn
+            TxnField.asset_receiver: Global.current_application_address(),
+            TxnField.xfer_asset: asset_id,
+            TxnField.asset_amount: Int(0),
+        },
+    )
+
+
+@Subroutine(TealType.none)
+def opt_out(asset_id: Expr, close_remainder_to: Expr) -> Expr:
+    """ optin single asset """
+    return InnerTxnBuilder.SetFields(
+        {
+            TxnField.type_enum: TxnType.AssetTransfer,
+            TxnField.fee: Int(0),  # cover fee with outer txn
+            TxnField.xfer_asset: asset_id,
+            TxnField.asset_amount: Int(0),
+            TxnField.asset_receiver: Global.creator_address(),
+            TxnField.asset_close_to: close_remainder_to
+        },
+    )
+
+
+@app.create(bare=True)
+def create() -> Expr:
+    # Set all global state to the default values
+    return app.initialize_global_state()
+
+
+@app.external(authorize=Authorize.only(app.state.manager.get()))
+def mint(url: abi.String, reserve: abi.Account, note: abi.String) -> Expr:
+    """ opt in assets """
+    return Seq(
+        ##########################################
+        # assertions
+        ##########################################
+        Assert(app.state.url.get() == Bytes("")),
+        Assert(app.state.reserve.get() == Global.creator_address()),
+        ##########################################
+        # state update
+        ##########################################
+        app.state.url.set(url.encode()),
+        app.state.url.set(note.encode()),
+        app.state.reserve.set(reserve.address()),
+        ##########################################
+    )
+
+# fees:
+# - requires fee 0.002A
+
+# @app.external(authorize=Authorize.only(app.state.manager.get()))
+
+
+def opt_in_asset(asset: abi.Asset) -> Expr:
+    """ opt in assets """
+    return Seq(
+        ##########################################
+        # inner txns
+        ##########################################
+        InnerTxnBuilder.Begin(),
+        opt_in(asset.asset_id()),
+        InnerTxnBuilder.Submit(),
+        ##########################################
+    )
+
+# fees:
+# - requires fee 0.002A
+
+
+@app.external(authorize=Authorize.only(app.state.manager.get()))
+def opt_out_asset(asset: abi.Asset, asset_receiver: abi.Account) -> Expr:
+    """ opt in assets """
+    return Seq(
+        ##########################################
+        # inner txns
+        ##########################################
+        InnerTxnBuilder.Begin(),
+        opt_out(asset.asset_id(), asset_receiver.address()),
+        InnerTxnBuilder.Submit(),
+        ##########################################
+    )
+
+# fees:
+# - requires fee 0.002A
+
+
+@app.external(authorize=Authorize.only(app.state.manager.get()))
+def withdraw(asset: abi.Asset, asset_amount: abi.Uint64, asset_receiver: abi.Account) -> Expr:
+    """ opt in assets """
+    return Seq(
+        ##########################################
+        # inner txns
+        ##########################################
+        InnerTxnBuilder.Begin(),
+        pay(asset_receiver.address(), asset_amount.get(), asset.asset_id()),
+        InnerTxnBuilder.Submit(),
+        ##########################################
+    )
+
+
+# fees:
+# - requires fee 0.002A
+
+@app.external(authorize=Authorize.only(app.state.manager.get()))
+def grant(manager: abi.Account) -> Expr:
+    """ update manager """
+    return Seq(
+        ##########################################
+        # state update
+        ##########################################
+        app.state.manager.set(manager.address())
+        ##########################################
+    )
+
+# fees:
+# - requires fee 0.002A
+# on complete:
+# - requires DeleteApplicationOC
+
+
+@app.delete
+def delete() -> Expr:
+    return Seq(
+        ###############################################
+        # inner txns
+        ###############################################
+        InnerTxnBuilder.Begin(),
+        free_app_mbr(),
+        InnerTxnBuilder.Submit()
+        ###############################################
+    )
+
+
+if __name__ == "__main__":
+    app.build().export()
